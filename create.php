f152c92fd6ac3bb3604fc276dbc9c04b75b37c90
11h00 <tpjdog@gmail.com>
2021-07-25 23:28:07 +0200

    decoding task and printing it

--- create.php
+++ create.php
@@ -1,7 +1,8 @@
 <?php
     if (isset($_POST)){
         if (isset($_POST["taskname"])){
-            
+            $task = json_decode(file_get_contents("task.json"), true);
+            print_r()
         }
     }
 ?>

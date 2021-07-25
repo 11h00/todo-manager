3edf1bc63046befd5b81e604266ccc4c398e9d71
11h00 <tpjdog@gmail.com>
2021-07-25 23:44:50 +0200

    changed task.json and fixed name of the ask

--- create.php
+++ create.php
@@ -2,7 +2,7 @@
     if (isset($_POST)){
         if (isset($_POST["taskname"])){
             $task = json_decode(file_get_contents("task.json"), true);
-            array_push($task["tasks"], ["name" => $_POST["taskname"], "id" => count($task["tasks"] + 1)]);
+            array_push($task["tasks"], ["task" => $_POST["taskname"], "id" => intval(count($task["tasks"]) + 1)]);
             $task = json_encode($task);
             file_put_contents("task.json", $task);
         }
